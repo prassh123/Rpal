505d2e7d1225135411ebe087a184f41afb6f350e
Prashanth Raghavan <r.prashanth@ymail.com>
2010-06-22 00:53:06 -0700

    code update with lexer

--- src/com/cise/ufl/rpal/Lexer.java
+++ src/com/cise/ufl/rpal/Lexer.java
@@ -2,6 +2,9 @@ package com.cise.ufl.rpal;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -10,6 +13,7 @@ import java.util.regex.Pattern;
 class Lexer<E> {
         private StringBuilder contents = new StringBuilder();
         private ArrayList<E> tokenList = new ArrayList<E> ();
+        private HashMap lexTable = new HashMap ();
        
         Logger logger = Logger.getLogger("rpalLogger");
         
@@ -39,20 +43,178 @@ class Lexer<E> {
         	}
         	
         	StringTokenizer st = new StringTokenizer (contents.toString(), " ");
+        	boolean status = false;
+        	E token = (E) "";
+        	//ArrayList queue = new ArrayList();
+        	
         	while (st.hasMoreTokens()) {
-        		E token = (E) st.nextToken();
-        		if (token.equals("<eol>") || token.equals ("\t")) 
+        		
+        		if (! status) {
+        			 token = (E) st.nextToken().trim();			// We only get the next token if the status is true. because we might have additional sequences of ((((( etc...
+        			 System.out.println ("About to classify " + token );
+        		}
+        		if (token.equals("<eol>") || token.equals ("\t")) {
+        			status = false;
         			continue;
+        		}
         		else if (((String) token).startsWith("//")) {
+        				status = false;
                			while (st.nextToken().equals("<eol>") != true)
         				continue;
         		}
-        		if (! ((String) token).startsWith("//"))
-        			tokenList.add(token);
+        		/* insert code to add it to the identifer list 
+        		 * Trial code by making use of lexemes
+        		 * 
+        		 * */
+        		else {
+        			StringBuffer sb = new StringBuffer ();
+        			String temp = (String) token;
+        			int i = 0;
+        			char operator = 0 ;
+        			boolean operatorFound = true;
+        			while ( i < temp.length() &&  (((String)token).charAt(i)) != 0 && !(isOperatorSymbol(new String(""+((String)token).charAt(i))))   && !isLeftBracketSymbol(new String(""+((String)token).charAt(i))) 
+        					    && !isRightBracketSymbol (new String(""+((String)token).charAt(i)))  &&  !isCommaOperatorSymbol(new String(""+((String)token).charAt(i))) && temp.length()>1
+        		        						)
+        			{
+        					operatorFound = true;
+        					System.out.println (temp.charAt(i));
+        					sb.append(((String)token).charAt(i));		// keep pushing the tokens in to the string buffer until we encounter any of the above...
+        					i++;
+        					if (i == temp.length()) {
+        						operatorFound = false;
+        						break;
+        					}
+        			}
+        			
+        			
+        			if (temp.length() == 1 && temp.charAt(0) != 0) {
+        				System.out.println ("Adding just one character" + temp.charAt(0));
+        				tokenList.add((E) temp);	
+        				status = false;
+        				continue;
+        			}
+        			if (operatorFound ) {
+        			
+        			    operator = ((String)token).charAt(i);
+        			   
+        			    if ( isOperatorSymbol(new String(""+operator) )|| 
+        			    	 isLeftBracketSymbol(new String(""+operator) ) || isRightBracketSymbol(new String(""+operator) ) || isCommaOperatorSymbol(new String(""+operator))) {
+        			        System.out.println ("Operator Found " +  operator);
+        			        if (sb.length()>0) {
+        			        	System.out.println ("Adding a complete token " + sb);
+        			        	tokenList.add((E) sb);
+        			        } 
+        			        if (operator == '-' && ((String)token).charAt(i+1) !=0 &&  ((String)token).charAt(i+1) == '>') {
+        			        	tokenList.add((E) "->");	
+        			        	System.out.println ("Token added ->");
+        			        }
+        			        else {
+        			        	System.out.println ("Adding Operator " + operator);
+        			        	tokenList.add( (E) new String(""+operator));
+        			        }
+        			       
+        			        if (  ((String)token).length() > 1 ) {
+        			            
+        			        	if (sb.length() > 0) {
+        			        	    token = (E) temp.substring(sb.length()+1);
+        			        	    System.out.println ("After reduction " + token);
+        			        	}
+        			        	else {
+        			        		token = (E) temp.substring(1);
+        			        		System.out.println ("After reduction " + token);
+        			        	}
+        			        	if (((String)token).length() > 0) { 
+        			            status = true;
+        			            continue;
+        			        	}
+        			        	else {
+        			        		status=false;
+        			        		continue;
+        			        	}
+        			        }
+        			    else {
+        			    	status=false;
+        			    	continue;
+        			    }
+        			    }
+        			    else { 
+        			    	System.out.println ("I think this is a complete  token " + sb);
+    			        	if (sb.length() > 0)
+        			    	    tokenList.add((E) sb);
+        			    	status=false;
+        			    }
+        			}
+        			else {
+        				
+        				System.out.println ("Operator not found, so jus adding " + sb);
+        				if (sb.length() > 0)
+        				    tokenList.add((E) sb);
+        				status = false;
+        				continue;
+        			}
+        		}
+        		/*
+        		 * end trial code
+        		 */
+ /*       		else if ( ((String)token).charAt(0)   == '(') {
+        			
+        			String temp = (String) token;
+        			tokenList.add( (E) (    new String (""+ ((String)temp).charAt(0) )));
+        			if (temp.length() > 1) {
+               			token = (E) temp.substring(1);			// we add the first occurance of '(' to the list and then let the while loop detect subsequent tokens by setting the status to false.
+        				status = true;
+        			}
+        			else {
+        				status = false;
+        				continue;
+        			}
+        			System.out.println ("Token is reduced to " + token);
+        			continue;
+        		}
+        		
+        		else if (((String) token).contains(",")) {		// for now, i am going to assume that ',' if it occurs would only occur at the end of the string.
+        			String temp = (String) token;
+        			tokenList.add( (E) (    new String (",")));
+        			if (temp.length() > 1) {
+        			token = (E) temp.substring(0, temp.length()-1 );
+        			status = true;
+        			}
+        			else {
+        				status = false;
+        				continue;
+        			}
+        				
+        			
+        		}
+        		else if ( ((String)token).charAt(((String)token).length()-1)   == ')') {
+        			
+        			String temp = (String) token;
+        			tokenList.add( (E) (    new String (""+ ((String)temp).charAt(((String)token).length()-1) )));
+        			if (temp.length() > 1) {
+        				status = true;
+            			token = (E) temp.substring(0, ((String)token).length()-1);			// we add the last occurance of ')' to the list and then let the while loop detect subsequent preceding tokens by setting the status to false.
+        			}
+        			else {
+        			//System.out.println ("Token is reduced to " + token);
+        				status = false;
+        				continue;
+        			}
+        			continue;
+        		}
+        		
+        		if (! ((String) token).startsWith("//")) {
+        			tokenList.add((E) ((String) token).trim());
+        			status = false;
+        		}*/
         	}
-        	logger.info("Token List :" + tokenList );
+        	System.out.println ("Token List :" + tokenList );
         }
-        
+        private boolean isArrow (String token ) {
+        	Pattern p = Pattern.compile("^->$");  // pattern to detect only the integers
+        	Matcher match = p.matcher(token);
+        	boolean result = match.find();
+        	return result;  	
+        }
         private boolean isInteger (String token) {
         	Pattern p = Pattern.compile("^[0-9]*$");  // pattern to detect only the integers
         	Matcher match = p.matcher(token);
@@ -65,27 +227,83 @@ class Lexer<E> {
         	boolean result = match.find();
         	return result;  	
         }
-        
-        public void getTypeOfToken (String token) {
-        	if ( isInteger (token))
-        		System.out.println( token + "\t" + "Integer");
-        	else if (isIdentifier(token))
-        		System.out.println( token + "\t" + "Identifier");
-        	else 
-        		System.out.println( token + "\t" + "Unknown");
+        public boolean isOperatorSymbol(String token) {
+        	Pattern p = Pattern.compile("^[-+*/<>&.@/:=~|$!#%^_\\[\\]{}\"'?]$");
+        	Matcher match = p.matcher(token);
+        	boolean result = match.find();
+			return result;
+		}
+        public boolean isLeftBracketSymbol (String token ) {
+        	Pattern p = Pattern.compile("^[(]$");
+        	Matcher match = p.matcher(token);
+        	boolean result = match.find();
+			return result;
+        }
+
+        public boolean isRightBracketSymbol (String token ) {
+        	Pattern p = Pattern.compile("^[)]$");
+        	Matcher match = p.matcher(token);
+        	boolean result = match.find();
+			return result;
         }
         
-        public void printLexTable () {
-        	for (E token: tokenList ) {
-        		this.getTypeOfToken((String) token);
+        public boolean isCommaOperatorSymbol (String token) {
+        	Pattern p = Pattern.compile("^[,]$");
+        	Matcher match = p.matcher(token);
+        	boolean result = match.find();
+			return result;
+        }
+        
+        public String getTypeOfToken (String token) {				// Note that the order of checking for various types is important...
+        	if ( isInteger (token)) {
+        		System.out.println( token + "\t" + "Integer");
+        		return "Integer";
         	}
+        	
+        	else if (isOperatorSymbol (token)) {
+        		System.out.println( token + "\t" + "Operator");
+        		return "Operator_symbol";
+        	}
+        	
+         	else if (isLeftBracketSymbol (token)) {
+        		System.out.println( token + "\t" + "(");
+        		return "(";
+        	}
+        	
+         	else if (isRightBracketSymbol (token)) {
+        		System.out.println( token + "\t" + ")");
+        		return ")";
+        	}
+        	
+         	else if (isCommaOperatorSymbol (token)) {
+        		System.out.println( token + "\t" + ",");
+        		return ",";
+        	}
+        	
+        	else if (isIdentifier(token)) {
+        		System.out.println( token + "\t" + "Identifier");
+        		return "Identifier";
+           	}
+        	
+        	else {
+        		System.out.println( token + "\t" + "Unknown");
+        		return "Unknown";
+        	}
+        }
+      
+		public void printLexTable () {
+        	for (E token: tokenList ) {
+        		lexTable.put((String) token, this.getTypeOfToken((String)token));
+        		//this.getTypeOfToken((String) token);
+        	}
+        	System.out.print(lexTable);
         }
         
         public static void main(String args[]) {
                 Lexer lexer = new Lexer ();
                 lexer.readFile (args[0]);
-                lexer.constructTokens();
-              //System.out.println("The result  " + lexer.isInteger("12345"));
+               lexer.constructTokens();
+               // System.out.println("The result  " + lexer.isArrow("-"));
                 lexer.printLexTable();
         }
 }
